5603c9cbd3b17686720b202ccdcf5068bde95c21
Justin Carlson <jrcarlson8@gmail.com>
2019-02-13 14:41:28 -0500

    Show image cover in track details.

--- lib/pages/track/track_details.dart
+++ lib/pages/track/track_details.dart
@@ -14,16 +14,36 @@ class _TrackDetailsPageState extends State<TrackDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TODO'),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Text('Track details - ${widget.trackId}'),
-          ],
+        appBar: AppBar(
+          title: Text('TODO'),
         ),
-      ),
-    );
+        body: SafeArea(
+          child: Column(children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image:
+                        NetworkImage('https://picsum.photos/800/800?image=100'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 8,
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Track details - ${widget.trackId}'),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ));
   }
 }
