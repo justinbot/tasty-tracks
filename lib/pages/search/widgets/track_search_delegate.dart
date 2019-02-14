fc6faf5abc6b52bd8f71d826affcc132fd93f45c
Justin Carlson <jrcarlson8@gmail.com>
2019-02-14 12:21:09 -0500

    Fetch full tracks for search results.

--- lib/pages/search/widgets/track_search_delegate.dart
+++ lib/pages/search/widgets/track_search_delegate.dart
@@ -5,6 +5,7 @@ import 'package:tasty_tracks/spotify_api.dart';
 
 class TrackSearchDelegate extends SearchDelegate {
   // TODO Implement searching as user types, with debouncing
+  // buildSuggestions
 
   @override
   ThemeData appBarTheme(BuildContext context) {
@@ -35,75 +36,69 @@ class TrackSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
+    // TODO Handle empty query
     // TODO cache results, only fetch if query changes
-    var searchResults = spotifyApi.search
+    // Get search results
+    Stream<List<spotify.Page<Object>>> searchResults = spotifyApi.search
         .get(query, [spotify.SearchType.track])
         .first(20)
         .asStream();
 
-    return Column(
-      children: <Widget>[
-        StreamBuilder(
-          stream: searchResults,
-          builder:
-              (context, AsyncSnapshot<List<spotify.Page<Object>>> snapshot) {
-            if (!snapshot.hasData) {
-              return Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Center(child: CircularProgressIndicator()),
-                ],
-              ));
-            } else if (snapshot.hasError) {
-              return Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('An error occured while searching :('),
-                ],
-              ));
-            } else {
-              // TODO Handle multiple pages
-              if (snapshot.data.isEmpty || snapshot.data.first.items.isEmpty) {
-                return Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('No results found :(',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.title),
-                  ],
-                ));
-              } else {
-                spotify.Page<Object> page = snapshot.data.first;
-                List<Object> results = page.items.toList();
-                return Expanded(
-                    child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: results.length,
-                  itemBuilder: (context, index) {
-                    var item = results[index];
-                    if (item is spotify.TrackSimple) {
-                      return _trackListItem(context, item);
-                    }
-                  },
-                ));
+    return StreamBuilder(
+      stream: searchResults,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error getting your results :('));
+        }
+
+        if (snapshot.data.isEmpty || snapshot.data.first.items.isEmpty) {
+          return Center(
+            child: Text('No results found :(',
+                style: Theme.of(context).textTheme.title),
+          );
+        }
+
+        spotify.Page<Object> page = snapshot.data.first;
+        List<Object> results = page.items.toList();
+        List<spotify.TrackSimple> trackResults =
+            results.whereType<spotify.TrackSimple>().toList();
+
+        // Get full tracks
+        var fullTracks =
+            spotifyApi.tracks.list(trackResults.map((t) => t.id)).asStream();
+
+        return StreamBuilder(
+            stream: fullTracks,
+            builder: (context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
               }
-            }
-          },
-        ),
-      ],
+
+              if (snapshot.hasError) {
+                return Center(child: Text('Error getting your results :('));
+              }
+
+              List<spotify.Track> tracksList = snapshot.data.toList();
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: tracksList.length,
+                itemBuilder: (context, index) {
+                  var item = tracksList[index];
+                  return _trackListItem(context, item);
+                },
+              );
+            });
+      },
     );
   }
 
-  Widget _trackListItem(BuildContext context, spotify.TrackSimple track) {
-    // TODO Use album art for avatar
+  Widget _trackListItem(BuildContext context, spotify.Track track) {
     CircleAvatar trackAvatar = CircleAvatar(
-      backgroundColor: Colors.brown.shade800,
+      backgroundImage: NetworkImage(track.album.images.first.url),
     );
     String artistNames = track.artists.map((artist) => artist.name).join(', ');
 
@@ -120,6 +115,7 @@ class TrackSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Column();
+    // return buildResults(context);
+    return Center(child: Text('Suggestions go here'));
   }
 }
