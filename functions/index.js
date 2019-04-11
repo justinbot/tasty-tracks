95e12c6afee8c623c504d609368918cbb792e590
Justin Carlson <jrcarlson8@gmail.com>
2019-04-11 13:27:54 -0400

    Placeholder Cloud Functions.

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,43 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.placeBet = functions.https.onRequest((request, response) => {
+    // trackId, wager
+
+    // Verify user has balance for wager
+    // TODO
+
+    // Get Track data from Spotify
+    // TODO
+
+    // Place bet with wager and current popularity
+    // TODO
+});
+
+exports.cancelBet = functions.https.onRequest((request, response) => {
+    // betId
+
+    // Get bet
+
+    // Get Track data from Spotify
+    // TODO
+
+    // Calculate outcome with wager and current popularity
+    // TODO
+
+    // Delete bet and add outcome to balance
+});
+
+exports.getBetOutcome = functions.https.onRequest((request, response) => {
+    // betId
+
+    // Get bet
+
+    // Get Track data from Spotify
+    // TODO
+
+    // Calculate outcome with wager and current popularity
+    // TODO
+});
+
