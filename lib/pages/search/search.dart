fbbb5fd52a926c5789e73b464d8cea937ca088e1
Justin Carlson <jrcarlson8@gmail.com>
2019-04-11 22:21:35 -0400

    Improve Search page loading.

--- lib/pages/search/search.dart
+++ lib/pages/search/search.dart
@@ -9,6 +9,7 @@ import 'package:tasty_tracks/pages/artist/artist.dart';
 import 'package:tasty_tracks/pages/search/music_search_delegate.dart';
 import 'package:tasty_tracks/pages/search/widgets/search_history.dart';
 import 'package:tasty_tracks/pages/track/track.dart';
+import 'package:tasty_tracks/widgets/error_page.dart';
 
 class SearchPage extends StatefulWidget {
   static final String routeName = '/search';
@@ -19,69 +20,91 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  List<spotify.Track> searchHistoryTracks = List();
-  List<spotify.Artist> searchHistoryArtists = List();
-  List<spotify.Album> searchHistoryAlbums = List();
+  bool _isBusy;
+  bool _hasError;
 
-  final searchHistoryModel = SearchHistoryModel();
+  SearchHistoryModel _searchHistoryModel;
+
+//  Iterable<spotify.Album> _albums;
+//  Iterable<spotify.Artist> _artists;
+//  Iterable<spotify.Track> _tracks;
 
   @override
   initState() {
     super.initState();
 
-    searchHistoryModel.loadItems();
+    _loadData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ScopedModel<SearchHistoryModel>(
-      model: searchHistoryModel,
-      child: Scaffold(
-        appBar: AppBar(
-          flexibleSpace: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: RaisedButton(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      FeatherIcons.search,
-                      size: 18.0,
-                    ),
-                    const SizedBox(width: 8.0),
-                    Text('Tracks, artists, or albums'),
-                  ],
+    if (_hasError) {
+      return Scaffold(
+        body: SafeArea(
+          child: ErrorPage(
+            errorText: 'Failed to load searches :(',
+            onRetry: () => _loadData(),
+          ),
+        ),
+      );
+    } else if (_isBusy) {
+      return Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      );
+    } else {
+      return ScopedModel<SearchHistoryModel>(
+        model: _searchHistoryModel,
+        child: Scaffold(
+          appBar: AppBar(
+            flexibleSpace: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        FeatherIcons.search,
+                        size: 18.0,
+                      ),
+                      const SizedBox(width: 8.0),
+                      Text('Tracks, artists, or albums'),
+                    ],
+                  ),
+                  color: Colors.white,
+                  onPressed: () {
+                    // TODO When navigating back from a search result, search bar should be active
+                    // TODO Consider not using showSearch for better behavior
+                    showSearch(
+                      context: context,
+                      delegate: MusicSearchDelegate(
+                        searchHistoryModel: _searchHistoryModel,
+                      ),
+                    ).then((selectedItem) {
+                      _handleSelectedItem(selectedItem);
+                    });
+                  },
                 ),
-                color: Colors.white,
-                onPressed: () {
-                  // TODO When navigating back from a search result, search bar should be active
-                  // TODO Consider not using showSearch for better behavior
-                  showSearch(
-                    context: context,
-                    delegate: MusicSearchDelegate(
-                      searchHistoryModel: searchHistoryModel,
-                    ),
-                  ).then((selectedItem) {
-                    _handleSelectedItem(selectedItem);
-                  });
-                },
               ),
             ),
           ),
+          body: SearchHistory(
+            onSelectedItem: _handleSelectedItem,
+          ),
         ),
-        body: SearchHistory(
-          onSelectedItem: _handleSelectedItem,
-        ),
-      ),
-    );
+      );
+    }
   }
 
   _handleSelectedItem(Object selectedItem) {
     if (selectedItem != null) {
       // Navigate to details page for selected item
       if (selectedItem is spotify.AlbumSimple) {
-        searchHistoryModel.addAlbum(selectedItem);
+        _searchHistoryModel.addAlbum(selectedItem);
         String imageUrl;
         if (selectedItem.images.isNotEmpty) {
           imageUrl = selectedItem.images.first.url;
@@ -96,7 +119,7 @@ class _SearchPageState extends State<SearchPage> {
           },
         );
       } else if (selectedItem is spotify.Artist) {
-        searchHistoryModel.addArtist(selectedItem);
+        _searchHistoryModel.addArtist(selectedItem);
         String imageUrl;
         if (selectedItem.images.isNotEmpty) {
           imageUrl = selectedItem.images.first.url;
@@ -111,7 +134,7 @@ class _SearchPageState extends State<SearchPage> {
           },
         );
       } else if (selectedItem is spotify.Track) {
-        searchHistoryModel.addTrack(selectedItem);
+        _searchHistoryModel.addTrack(selectedItem);
         String imageUrl;
         if (selectedItem.album.images.isNotEmpty) {
           imageUrl = selectedItem.album.images.first.url;
@@ -127,5 +150,37 @@ class _SearchPageState extends State<SearchPage> {
         );
       }
     }
+  }
+
+  void _loadData() async {
+    setState(() {
+      _isBusy = true;
+      _hasError = false;
+    });
+
+    SearchHistoryModel searchHistoryModel = SearchHistoryModel();
+    await searchHistoryModel.loadItems();
+
+//    Iterable<spotify.Album> albums;
+//    Iterable<spotify.Artist> artists;
+//    Iterable<spotify.Track> tracks;
+//    try {
+//      albums = await searchHistoryModel.albumItems();
+//      artists = await searchHistoryModel.artistItems();
+//      tracks = await searchHistoryModel.trackItems();
+//    } catch (e) {
+//      // TODO Log to error reporting
+//      setState(() {
+//        _hasError = true;
+//      });
+//    }
+
+    setState(() {
+      _isBusy = false;
+      _searchHistoryModel = searchHistoryModel;
+//      _albums = albums;
+//      _artists = artists;
+//      _tracks = tracks;
+    });
   }
 }
